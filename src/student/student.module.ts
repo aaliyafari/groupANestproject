12b2266679ab3b9cbea62cfc784527d0d9fd1ff7
Sonali-Mali93 <sonalimali999@gmail.com>
2022-08-16 11:38:40 +0530

    feat: Student servvice is created

--- src/student/student.module.ts
+++ src/student/student.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StudentController } from './controllers/student.controller';
+import { StudentService } from './services/student.service';
 
 @Module({
-  controllers: [StudentController]
+  controllers: [StudentController],
+  providers: [StudentService]
 })
 export class StudentModule {}
