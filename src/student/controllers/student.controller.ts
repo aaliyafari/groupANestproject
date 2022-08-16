42e30d082221f9206b468cb6e1010c405337379c
Sonali-Mali93 <sonalimali999@gmail.com>
2022-08-16 15:44:55 +0530

    feat: Api for getting all student record

--- src/student/controllers/student.controller.ts
+++ src/student/controllers/student.controller.ts
@@ -1,13 +1,27 @@
-import { Get, Post, Body, Controller } from '@nestjs/common';
+import {
+  Get,
+  Post,
+  Body,
+  Controller,
+  Param,
+  Put,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { StudentData } from '../models/student.interface';
 import { StudentDataEntity } from '../models/student.entity';
 import { StudentService } from '../services/student.service';
 import { Observable } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
 @Controller('student')
 export class StudentController {
   constructor(private studentService: StudentService) {}
   @Post()
   InsertStudentData(@Body() studentData: StudentData): Observable<StudentData> {
     return this.studentService.createStudentRecord(studentData);
+  }
+  @Get()
+  getAllStudentData(): Observable<StudentData[]> {
+    return this.studentService.getAllStudents();
   }
 }
