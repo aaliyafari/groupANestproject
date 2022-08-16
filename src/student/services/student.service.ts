008fa8041d9fbd7c70cfc72f4758a7fb1fe65734
Sonali-Mali93 <sonalimali999@gmail.com>
2022-08-16 15:45:52 +0530

    feat: service api for getting all student record

--- src/student/services/student.service.ts
+++ src/student/services/student.service.ts
@@ -3,7 +3,7 @@ import { isPort } from 'class-validator';
 import { StudentData } from '../models/student.interface';
 import { StudentDataEntity } from '../models/student.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { from, Observable } from 'rxjs';
 @Injectable()
 export class StudentService {
@@ -11,10 +11,12 @@ export class StudentService {
     @InjectRepository(StudentDataEntity)
     private readonly StudentDataRepository: Repository<StudentDataEntity>,
   ) {}
-
+  //Insert student data
   createStudentRecord(student_Data: StudentData): Observable<StudentData> {
     return from(this.StudentDataRepository.save(student_Data));
   }
-
- 
+  //get All students Data
+  getAllStudents() {
+    return from(this.StudentDataRepository.find());
+  }
 }
