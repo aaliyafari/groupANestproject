a591e6c7453765b39e7ec9bfd90c946fbea52976
Aaliya Farhin <aaliyafarhin93@gmail.com>
2022-09-01 16:36:45 +0530

    Feat:third entityy

--- src/usermodule/services/users.service.ts
+++ src/usermodule/services/users.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../typeorm/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '../dtos/CreateUser.dtos';
-import { Role } from '../typeorm/role.entity';
+
 
 
 @Injectable()
