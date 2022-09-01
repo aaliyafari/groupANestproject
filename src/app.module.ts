8067f0d7249411be4e103e0d18009e3cf5f1eaf8
Rajat <rajatdoijad67@gmail.com>
2022-09-01 18:26:51 +0530

    Fix:conflicts added

--- src/app.module.ts
+++ src/app.module.ts
@@ -23,7 +23,7 @@ import { UsersModule } from './usermodule/users.module';
         type: 'postgres',
         host: configService.get('DB_HOST'),
         port: +configService.get<number>('DB_PORT'),
-       // username: configService.get('DB_USERNAME'),
+       username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
