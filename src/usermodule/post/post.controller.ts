6e769c47b6ed55b539862f421dd7b89db9a30b5f
Aaliya Farhin <aaliyafarhin93@gmail.com>
2022-09-01 17:34:28 +0530

    Feat:chnages done to the feat

--- src/usermodule/post/post.controller.ts
+++ src/usermodule/post/post.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+
 
 @Controller('post')
 export class PostController {
@@ -22,10 +22,7 @@ export class PostController {
     return this.postService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
-  }
+
 
   @Delete(':id')
   remove(@Param('id') id: string) {
