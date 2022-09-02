92e2e6436b96627357876006cf578124dbbac316
arnabbis <biswas.anuska.2004@gmail.com>
2022-09-02 15:14:06 +0530

    feat: changes in the code

--- src/product/controllers/product-category.controller.ts
+++ src/product/controllers/product-category.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Req,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { DeleteResult, UpdateResult } from 'typeorm';
 //import { ProductPostCategory } from '../models/product-category.entity';
-import { productOneToMany} from '../models/product-category.interface';
+import { productOneToMany } from '../models/product-category.interface';
 import { product_model } from '../models/product-category.model';
 import { oneToMany_service } from '../services/product-category.service';
 //import { ProductCategoryModel } from '../models/product-category.model';
@@ -11,26 +20,20 @@ import { oneToMany_service } from '../services/product-category.service';
 // import { ProductPostEntity } from '../models/product.entity';
 //import { ProductCategoryService } from '../services/product-category.service';
 
-@Controller("size")
+@Controller('size')
 export class ProductSizeController {
-    constructor(private productSizeService: oneToMany_service) { }
-    @Get()
-    hello() {
-        return "hiii amit"
-    }
-    @Post()
+  constructor(private productSizeService: oneToMany_service) {}
+  @Get()
+  hello() {
+    return 'hiii amit';
+  }
+  @Post()
+  createsize(@Body() productSize: product_model): Observable<productOneToMany> {
+    return this.productSizeService.createproductsize(productSize);
+  }
 
-    createsize(@Body() productSize: product_model): Observable<productOneToMany> {
-        return this.productSizeService.createproductsize(productSize)
-    }
-
-    @Get("/allData")
-    allData(): Observable<productOneToMany[]> {
-
-        return this.productSizeService.findAllData()
-    }
-
-
-
-
+  @Get('/allData')
+  allData(): Observable<productOneToMany[]> {
+    return this.productSizeService.findAllData();
+  }
 }
